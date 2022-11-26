355e54c82ebb9f1e8d31ca50ded9865ad12c3972
ohmyoungjin <sisaudwls@naver.com>
2022-11-26 19:54:15 +0900

    20221126 :: Spring-MVC-Exception servlet Exception

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -1,16 +1,22 @@
 package hello.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+
+//Bean으로 등록해줘야 한다.
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
 
         //Spring boot가 제공하는 lib
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/400");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         //오류 페이지는 예외를 다룰 때 해당 예외와 그 자식 타입의 오류를 함께 처리한다. 예를 들어서 위의 경우
         //RuntimeException 은 물론이고 RuntimeException 의 자식도 함께 처리한다.
@@ -19,4 +25,5 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
 
     }
+
 }
