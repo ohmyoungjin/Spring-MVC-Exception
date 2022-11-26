69ded5a1a94f5352b37d2a91bc8b8b5ef963981d
ohmyoungjin <sisaudwls@naver.com>
2022-11-26 20:14:54 +0900

    20221126 :: Spring-MVC-Exception servlet Exception - filter

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        //해당 하는 부분은 REQUEST, ERROR에 둘 다 호출이 되도록 설정
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterRegistrationBean;
+    }
+}
